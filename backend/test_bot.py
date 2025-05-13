fe9d97990dbcdb62516ac0439cc66af9dfe227ff
bokovm <mbvlog07@gmail.com>
2025-05-13 14:10:22 +0300

    testbot

--- backend/test_bot.py
+++ backend/test_bot.py
@@ -1,6 +1,6 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
+﻿from telegram.ext import ApplicationBuilder, CommandHandler
 
-TOKEN = "7971445045:AAHUZQl_0NKNZlxip2QFSePSliaU0qdiBLA"  # Замените на реальный токен
+TOKEN = "123"  # Замените на реальный токен
 
 async def start(update, context):
     await update.message.reply_text("Бот работает!")
